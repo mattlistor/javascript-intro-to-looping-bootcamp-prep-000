86d08045d54e71217963b6ec81d899d86051dda2
Matthew Listor <mattlistor@gmail.com>
2019-04-20 17:01:50 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,11 +1,10 @@
 function forLoop(array) {
   for (let i = 0; i < 25; i++) {
-    if (i == 1){
+    if (i == 0){
       array[i] = "I am 1 strange loop."
     }
     else {
-      array[i] = "I am ${i} strange loops."
+      array[i] = "I am ${i+1} strange loops."
     }
   }
-  return array
 }
