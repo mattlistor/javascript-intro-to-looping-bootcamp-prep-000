c452e8fc2c0c226b872fc78a95ed0fa55b966b36
Matthew Listor <mattlistor@gmail.com>
2019-04-20 16:47:01 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -3,12 +3,12 @@ for (var i = 1; i < 100; i++) {
 }
 
 function forLoop(array) {
-  for (let i = 1; i <= 25; i++) {
-    if (i == 1){
+  for (let i = 0; i < 25; i++) {
+    if (i == 0){
       array[i] = "I am 1 strange loop."
     }
     else {
-      array[i] = "I am ${i} strange loops."
+      array[i] = "I am ${i+1} strange loops."
     }
     return array
   }
